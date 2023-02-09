e36d351ce024d4e1c9a3417cf225e6ca6f1cabf8
calcado <pecalcado@gmail.com>
2023-02-08 21:53:53 -0300

    tests games

---
+++ tests/helpers.ts
@@ -0,0 +1,7 @@
+import prisma from "../src/config/database"
+
+export async function cleanDb(){
+await prisma.game.deleteMany({})    
+await prisma.console.deleteMany({})
+
+}
--- tests/integration/games.test.ts
+++ tests/integration/games.test.ts
@@ -1,4 +1,30 @@
 import app from "app"
+import prisma from "config/database"
+import httpStatus from "http-status"
 import supertest from "supertest"
+import createGame from "../factories/games-factory"
+import { cleanDb } from "../helpers"
+
+beforeAll(async ()=>{
+    await cleanDb();
+})
+
+beforeEach(async()=>{
+    await cleanDb();
+})
 
 const api = supertest(app)
+// get - getId/ post
+
+describe("GET / games", ()=>{
+
+    it("Should responde with status 200 and games body", async ()=>{
+        const createdGame = await createGame()
+
+        const result = await app.get("/games")
+
+        expect(result.status).toEqual(httpStatus.OK)
+        expect(result.body).toEqual([createdGame])
+    })
+
+})

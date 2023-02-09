4b58b98056dfbdc214c20302956ca934baa80238
calcado <pecalcado@gmail.com>
2023-02-08 22:29:50 -0300

    feat: games tests

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,7 +1,8 @@
 import prisma from "../src/config/database"
 
-export async function cleanDb(){
+export default async function cleanDb(){
 await prisma.game.deleteMany({})    
 await prisma.console.deleteMany({})
 
 }
+
--- tests/integration/games.test.ts
+++ tests/integration/games.test.ts
@@ -1,30 +1,67 @@
-import app from "app"
-import prisma from "config/database"
-import httpStatus from "http-status"
-import supertest from "supertest"
-import createGame from "../factories/games-factory"
-import { cleanDb } from "../helpers"
+import app from "app";
+import { faker } from "@faker-js/faker";
+import httpStatus from "http-status";
+import supertest from "supertest";
+import createConsole from "../factories/consoles-factory";
+import createGame from "../factories/games-factory";
+import  cleanDb  from "../helpers";
 
-beforeAll(async ()=>{
-    await cleanDb();
-})
 
-beforeEach(async()=>{
-    await cleanDb();
-})
+beforeAll(async () => {
+  await cleanDb();
+});
 
-const api = supertest(app)
+beforeEach(async () => {
+  await cleanDb();
+});
+
+const api = supertest(app);
 // get - getId/ post
 
-describe("GET / games", ()=>{
+describe("GET / games", () => {
+  it("Should respond with status 200 and games body", async () => {
+    const createdGame = await createGame();
 
-    it("Should responde with status 200 and games body", async ()=>{
-        const createdGame = await createGame()
+    const result = await api.get("/games");
 
-        const result = await app.get("/games")
+    expect(result.status).toEqual(httpStatus.OK);
+    expect(result.body).toEqual([createdGame]);
+  });
+});
 
-        expect(result.status).toEqual(httpStatus.OK)
-        expect(result.body).toEqual([createdGame])
-    })
+describe("POST / games", () => {
+  it("Should respond with status 201 and created game", async () => {
+    const console = await createConsole();
+    const game = {
+      title: faker.name.firstName(),
+      consoleId: console.id,
+    };
 
-})
+    const result = await api.post("/game").send(game);
+
+    expect(result.status).toEqual(httpStatus.CREATED);
+    expect(result.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          title: game.title,
+          consoleId: game.consoleId,
+        }),
+      ])
+    );
+  });
+  it("Should respond with status 409 when conflicted", async()=>{
+    const createdGame = await createGame()
+    const newGame = 
+        {
+            title: createdGame.title,
+            consoleId: createdGame.consoleId
+        }
+    
+
+    const response = await api.post("games").send(newGame)
+    
+    expect(response.status).toEqual(httpStatus.CONFLICT)
+
+    
+  })
+});
